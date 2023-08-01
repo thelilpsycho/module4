7d8820a3ab1619ce3202153e37d32141574bf54d
psycho <the.lilpsycho@vk.com>
2023-08-01 19:35:47 +0300

    Дз к уроку 8

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Advertisement(models.Model):
+
+#чарфилд - небольшое текст поле
+#заголовокчек
+ title = models.CharField("Заголовок", max_length=128)
+
+#описание
+ description = models.TextField("Описание")
+
+#цена
+#децималфилд - число с фикс точкой
+ price = models.DecimalField("Цена", max_digits=10,decimal_places=2)
+
+#уместен ли торг
+ auction = models.BooleanField("Торг", help_text="Уместен ли торг")
+
+#дата создания
+ created_at = models.DateTimeField(auto_now_add=True)
+
+#дата изменения
+ updated_at = models.DateTimeField(auto_now=True)
+
+ class Meta:
+  db_table='advertisements'
+  
+#изображение
+
+#адрес
+
+#отзывы о продавце
+
+#контакты
+
+#похожее
+
