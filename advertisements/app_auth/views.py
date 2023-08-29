14baa8280b129f999c5a8e1cf859fbe5b829eb17
psycho <the.lilpsycho@vk.com>
2023-08-29 19:56:43 +0300

    дз 10

---
+++ advertisements/app_auth/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+
+
+def profile_view(request):
+    return render(request, 'app_auth/profile.html')
+
+
+def login_view(request):
+    redirect_url=reverse('profile')
+
+    if request.method == 'GET':
+        if request.user.is_authentificated:
+            return redirect(redirect_url)
+        else:
+            return render(request, 'app_auth/login.html')
+        
+    username=request.POST['username']
+    password=request.POST['password']
+
+    user= authenticate(request, username=username, password=password)
+
+    if user is not None:
+        login(request, user)
+        return redirect(redirect_url)
+    return render(request, 'app_auth/login.html')
+
+def register_view(request):
+    return render(request, 'app_auth/login.html')
+
+def logout_view(request):
+
+    logout(request)
+    return redirect(reverse('login'))
